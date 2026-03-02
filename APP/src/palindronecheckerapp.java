ea48def6eb2be95c0062ec788028de1a2928819a
sumithkumardoddala <sumithkumardoddala@gmail.com>
2026-03-02 10:53:50 +0530

    UC9 created

--- APP/src/palindronecheckerapp.java
+++ APP/src/palindronecheckerapp.java
@@ -1,76 +1,33 @@
 import java.util.Scanner;
 
-class Node {
-    char data;
-    Node next;
+public class Recursivepalindronecheckerapp {
 
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+    // Recursive function
+    public static boolean isPalindrome(String str, int start, int end) {
 
-public class LinkedListpalindroncheckerapp {
-
-    // Function to check palindrome
-    public static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null)
+        // Base condition
+        if (start >= end)
             return true;
 
-        Node slow = head;
-        Node fast = head;
+        // If characters do not match
+        if (str.charAt(start) != str.charAt(end))
+            return false;
 
-        // Find middle using fast & slow pointer
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Reverse second half
-        Node prev = null;
-        Node current = slow;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-
-        // Compare both halves
-        Node firstHalf = head;
-        Node secondHalf = prev;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Linked List Based palindronecheckerapp");
+        System.out.println("UC9: Recursive palindronecheckerapp");
         System.out.print("Input: ");
+
         String input = sc.nextLine();
 
-        // Create Linked List
-        Node head = null, tail = null;
-        for (int i = 0; i < input.length(); i++) {
-            Node newNode = new Node(input.charAt(i));
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
+        boolean result = isPalindrome(input, 0, input.length() - 1);
 
-        if (isPalindrome(head))
+        if (result)
             System.out.println("It is a Palindrome.");
         else
             System.out.println("It is NOT a Palindrome.");
