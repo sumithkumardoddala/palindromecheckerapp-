187c8e3e519392ffc72b4c7f72659c6978c1d74d
sumithkumardoddala <sumithkumardoddala@gmail.com>
2026-03-02 10:57:07 +0530

    UC10 created

--- APP/src/palindronecheckerapp.java
+++ APP/src/palindronecheckerapp.java
@@ -1,31 +1,34 @@
 import java.util.Scanner;
 
-public class Recursivepalindronecheckerapp {
+public class CaseInsensitivePalindrome {
 
-    // Recursive function
-    public static boolean isPalindrome(String str, int start, int end) {
+    public static boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
 
-        // Base condition
-        if (start >= end)
-            return true;
-
-        // If characters do not match
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-
-        // Recursive call
-        return isPalindrome(str, start + 1, end - 1);
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("UC9: Recursive palindronecheckerapp");
+        System.out.println("UC10: Case-Insensitive & Space-Ignored Palindrome");
         System.out.print("Input: ");
 
         String input = sc.nextLine();
 
-        boolean result = isPalindrome(input, 0, input.length() - 1);
+        // Normalize string
+        String normalized = input
+                .toLowerCase()                // ignore case
+                .replaceAll("\\s+", "");      // remove spaces
+
+        boolean result = isPalindrome(normalized);
 
         if (result)
             System.out.println("It is a Palindrome.");
