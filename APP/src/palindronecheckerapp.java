4441e772cdc66128acb7f507a1c4ce3411a23b15
sumithkumardoddala <sumithkumardoddala@gmail.com>
2026-03-02 10:25:17 +0530

    UC2 created

--- APP/src/palindronecheckerapp.java
+++ APP/src/palindronecheckerapp.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class palindronecheckerapp {
+    public static void main(String[] args){
+        System.out.println("palindronecheckerapp");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+        boolean isPalindrome = true;
+        for(int i = 0; i < input.length() / 2; i++){
+            if(input.charAt(i) != input.charAt(input.length() - 1 - i)){
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
+    }
 }
