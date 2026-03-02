572b182ee22dff677026a54ae333f301ba1aaaac
sumithkumardoddala <sumithkumardoddala@gmail.com>
2026-03-02 10:35:18 +0530

    UC4 created

--- APP/src/palindronecheckerapp.java
+++ APP/src/palindronecheckerapp.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
-public class palindronecheckerapp {
+
+public class palinedronecheckerapp{
     public static void main(String[] args){
         System.out.println("palindronecheckerapp");
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Input: ");
         String input = sc.nextLine();
-        String reversed = "";
-        for(int i = input.length() - 1; i >= 0; i--){
-            reversed += input.charAt(i);
+
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end){
+            if (chars[start] != chars[end]){
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        if(input.equals(reversed)){
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not a Palindrome");
-        }
+
+        if (isPalindrome)
+            System.out.println("It is a Palindrome.");
+        else
+            System.out.println("It is NOT a Palindrome.");
+
         sc.close();
     }
 }
